5c8c062d26c84feedde44440ebd94d3da1764dac
Breno Carvalho <brenocarvalho709@gmail.com>
2022-11-22 00:33:34 -0300

    analytics added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import "@fontsource/poppins";
+import { Analytics } from "@vercel/analytics/react";
 
 const theme = extendTheme({
   fonts: {
@@ -11,8 +12,11 @@ const theme = extendTheme({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+      <Analytics />
+    </>
   );
 }
