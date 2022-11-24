1a0d1e73e60ce28af26aec44642dc4d1a3b78c1f
Breno Carvalho <brenocarvalho709@gmail.com>
2022-11-23 22:00:57 -0300

    test request authentication

--- src/pages/api/registerUser.ts
+++ src/pages/api/registerUser.ts
@@ -1,36 +1,63 @@
 import { createHmac } from "crypto";
+import { buffer } from "micro";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const WEBHOOK_SECRETKEY = process.env.WEBHOOK_SECRETKEY;
 
-function hmac_signature(body: object) {
-  const payload = JSON.stringify(body);
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
 
+function hmac_signature(bodyRaw: any) {
   const hmac = createHmac("sha256", WEBHOOK_SECRETKEY ? WEBHOOK_SECRETKEY : "")
-    .update(payload)
+    .update(bodyRaw)
     .digest("base64");
 
   return hmac;
 }
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+async function isAuthenticated(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  bodyRaw: any
+) {
+  const signatureWebhook = req.headers["x-yampi-hmac-sha256"];
+  const hmacSignature = hmac_signature(bodyRaw);
+
+  if (signatureWebhook == hmacSignature) {
+    return true;
+  } else {
+    res.status(401).json({ error: "Unauthorized" });
+    return false;
+  }
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method === "POST") {
     if (!WEBHOOK_SECRETKEY) {
       res.status(500).json({ error: "Invalid WEBHOOK_SECRETKEY" });
       return;
     }
 
-    const signatureWebhook = req.headers["x-yampi-hmac-sha256"];
-    const hmacSignature = hmac_signature(req.body);
+    var bodyRaw = await buffer(req);
 
-    console.log(signatureWebhook, hmacSignature);
+    if (!(await isAuthenticated(req, res, bodyRaw))) return;
 
-    if (signatureWebhook == hmacSignature) {
-      console.log("Authenticated");
-      res.status(200).json({ status: "Successfuly" });
-    } else {
-      console.log("Unauthorized");
-      res.status(401).json({ status: "Unauthorized" });
-    }
+    const bodyData = JSON.parse(bodyRaw.toString());
+
+    const user = {
+      name: bodyData.resource.customer.data.name,
+      email: bodyData.resource.customer.data.email,
+      cpf: bodyData.resource.customer.data.cpf,
+      phone: 55 + bodyData.resource.customer.data.phone.full_number,
+    };
+
+    console.log(user);
+    res.status(200).json({ status: "Successfuly" });
   }
 }
