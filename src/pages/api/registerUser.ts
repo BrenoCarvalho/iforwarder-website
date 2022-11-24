49f2f6217921b0e5030a0bb45874b92f67ebcca3
Breno Carvalho <brenocarvalho709@gmail.com>
2022-11-24 02:54:42 -0300

    fix duplicate email

--- src/pages/api/registerUser.ts
+++ src/pages/api/registerUser.ts
@@ -356,18 +356,19 @@ async function isAuthenticated(
   }
 }
 
-interface user {
+interface IUser {
   full_name: string;
   first_name: string;
   email: string;
   cpf: number;
   phone: number;
+  cart_token: string;
 }
 
 async function getUser(
   res: NextApiResponse,
   supabase: SupabaseClient<any, "public", any>,
-  user: user
+  user: IUser
 ) {
   const response = await supabase
     .from("Users")
@@ -384,7 +385,7 @@ async function getUser(
   return response.data[0].id;
 }
 
-async function createUser(res: NextApiResponse, user: user) {
+async function createUser(res: NextApiResponse, user: IUser) {
   const supabase = createClient(
     process.env.SUPABASE_URL ? process.env.SUPABASE_URL : "",
     process.env.SUPABASE_KEY ? process.env.SUPABASE_KEY : ""
@@ -398,6 +399,7 @@ async function createUser(res: NextApiResponse, user: user) {
       phone1: user.phone,
       access: "Full",
       enable: true,
+      cart_token: user.cart_token,
     },
   ]);
 
@@ -410,7 +412,7 @@ async function createUser(res: NextApiResponse, user: user) {
   return license ? license : false;
 }
 
-async function sendEmail(user: user, license: string) {
+async function sendEmail(user: IUser, license: string) {
   let transporter = nodemailer.createTransport({
     host: process.env.UMBLER_HOST,
     port: process.env.UMBLER_PORT,
@@ -449,18 +451,19 @@ export default async function handler(
       return;
     }
 
-    const user = {
+    const user: IUser = {
       full_name: bodyData.resource.customer.data.name,
       first_name: bodyData.resource.customer.data.first_name,
       email: bodyData.resource.customer.data.email,
       cpf: bodyData.resource.customer.data.cpf,
       phone: 55 + bodyData.resource.customer.data.phone.full_number,
+      cart_token: bodyData.resource.cart_token,
     };
 
     const license = await createUser(res, user);
     if (!license) return;
 
-    await sendEmail(user, "425897523478");
+    await sendEmail(user, license);
     res.status(200).json({ status: "Successfuly" });
   }
 }
